4e5f019d2217074c3b65190ad7bedcb1a897894f
mike100-saferidehealth <michael.guerrero@saferidehealth.com>
2023-10-16 12:20:49 -0500

    customized for me

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Avatar() {
     return (
-        <Image src="https://avatars.githubusercontent.com/u/68690233" alt="Avatar" width={100} height={100} className="rounded-full"/>
+        <Image src="https://avatars.githubusercontent.com/u/9091130" alt="Avatar" width={100} height={100} className="rounded-full"/>
     )
 }
--- config.js
+++ config.js
@@ -3,68 +3,28 @@ export const config = {
     bgColor: '#18181b',
     textColor: '#d4d4d8',
     iconColor: '#d4d4d8',
-    name: 'ashish',
-    description: 'solo developer by day, overthinker by night.',
+    name: 'Mike100',
+    description: 'Hello World.',
     links: [
         {
             slug: 'github',
             type: 'url',
-            link: 'https://github.com/asrvd/'
+            link: 'https://github.com/paradeofbears/'
         },
         {
             slug: 'discord',
             type: 'hover',
-            text: 'asheeshh#4872'
+            text: 'NA#4872'
         },
         {
             slug: 'twitter',
             type: 'url',
-            link: 'https://twitter.com/_asheeshh'
+            link: 'https://twitter.com/namenumeral'
         },
         {
             slug: 'instagram',
             type: 'url',
-            link: 'https://instagram.com/asrvd_'
-        },
-        {
-            slug: 'spotify',
-            type: 'url',
-            link: 'https://open.spotify.com/user/31ma7274fwzhcig5iibz25thypoi?si=b8e4d9aaa0e7425e'
-        },
-        {
-            slug: 'devdotto',
-            type: 'url',
-            link: 'https://dev.to/asheeshh'
-        },
-        {
-            slug: 'lastdotfm',
-            type: 'url',
-            link: 'https://last.fm/user/asheeshh'
-        },
-        {
-            slug: 'kitsu',
-            type: 'url',
-            link: 'https://kitsu.io/users/1258735'
-        },
-        {
-            slug: 'buymeacoffee',
-            type: 'url',
-            link: 'https://www.buymeacoffee.com/asheeshh'
-        },
-        {
-            slug: 'minutemailer',
-            type: 'url',
-            link: 'mailto:asheeshh@duck.com'
-        },
-        {
-            slug: 'npm',
-            type: 'url',
-            link: 'https://www.npmjs.com/~asheeshh'
-        },
-        {
-            slug: 'reddit',
-            type: 'url',
-            link: 'https://reddit.com/u/asheeeshh'
-        }
+            link: 'https://instagram.com/paradeofbears'
+        }  
     ]
 }
